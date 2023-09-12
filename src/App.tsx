c1d24a30ea8e3e02b26648887eff93994deaf62f
Rodrigo Silva <rodrigo-cleiton@hotmail.com>
2023-09-12 19:45:55 -0300

    feat: initial setup project layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { Button } from "./components/ui/button";
+import { useState } from 'react'
+
 export function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  function toggleTheme() {
+    setDarkMode(!darkMode)
+
+    const root = document.documentElement;
+
+    if (darkMode) {
+      root.classList.remove('dark');
+    } else {
+      root.classList.add('dark');
+    }
+  }
 
   return (
-    <h1>
-      Hello World
-    </h1>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl">
+          upload.ai
+        </h1>
+
+        <div>
+          <span>Desenvolvido por Rodrigo Silva</span>
+          <Button>
+            Github
+          </Button>
+        </div>
+      </div>
+
+    </div>
   )
 }
